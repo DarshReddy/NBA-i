685c7ef57647e635d3fd22efbf70d4a5ead5a30b
Darsh Reddy <darshan.reddy@practo.com>
2021-11-11 11:35:28 +0530

    Modified error handling for coil image loading

--- app/src/main/java/com/example/round2/assignment/view/PlayerCardsAdapter.kt
+++ app/src/main/java/com/example/round2/assignment/view/PlayerCardsAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.round2.assignment.view
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import coil.request.ImageRequest
 import com.example.round2.assignment.BR
 import com.example.round2.assignment.R
 import com.example.round2.assignment.data.models.Data
@@ -15,7 +17,7 @@ class PlayerCardsAdapter(
     private val onCardClicked: (player: Data) -> Unit
 ): PagingDataAdapter<Data, PlayerCardsAdapter.PlayerCardsViewHolder>(DataDifferentiator) {
 
-    private val randomImgUrl = "https://source.unsplash.com/random/200x200?sig="
+    private val playerImageUrl = "https://nba-players.herokuapp.com/players/"
 
     class PlayerCardsViewHolder(
         val binding: ItemPlayerCardBinding
@@ -35,9 +37,12 @@ class PlayerCardsAdapter(
             val playerData = getItem(position)
             setVariable(BR.player, playerData)
             if (playerData != null) {
-                playerData.imageUrl = randomImgUrl+position
+                playerData.imageUrl = playerImageUrl+"${playerData.last_name}/${playerData.first_name}"
                 playerImage.load(playerData.imageUrl) {
-                    placeholder(R.drawable.ic_baseline_account_box_24)
+                    listener(
+                        onError = { _: ImageRequest, _: Throwable ->
+                            playerImage.load(R.drawable.ic_baseline_account_box_24) }
+                    )
                 }
                 root.setOnClickListener {
                     onCardClicked(playerData)
